053f8131d34d3b612395870329ae3c6595b5e168
Jack Cattanach <jackcattanach@live.com>
2018-12-06 19:41:33 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './components/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Jack', age: 24},
+      {name: 'John', age: 26},
+      {name: 'Max', age: 28}
+    ],
+    showPersons: false
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1)
+    this.setState({persons: persons})
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({persons: [
+      {name: 'Jack', age: 24},
+      {name: 'John', age: 27},
+      {name: event.target.value, age: 28}
+      ]
+    })
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({showPersons: !doesShow})
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+    let persons = null
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
+        {this.state.persons.map((person, index )=> {
+          return <Person
+            click={() => this.deletePersonHandler(index)}
+            name={person.name}
+            age={person.age} />
+        })}
+      </div>
+      )
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App</h1>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Toggle Names</button>
+        {persons}
       </div>
     );
   }
